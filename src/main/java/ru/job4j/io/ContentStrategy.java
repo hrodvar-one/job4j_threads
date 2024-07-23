c7b5f58aee5d279b1d799459851e43dfa52afc13
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-23 08:49:25 +0600

    1. Visibility. Общий ресурс вне критической секции [#1102 #506211]

--- src/main/java/ru/job4j/io/ContentStrategy.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.io;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface ContentStrategy {
-    String getContent(InputStream input) throws IOException;
-}
