83afb799b1ef88dfbc5a1c1493bd418650d34f86
Aleksandr Kobelskiy <kobel.san.sanich@gmail.com>
2024-07-27 17:58:07 +0600

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,41 @@
+package ru.job4j.buffer;
+
+import ru.job4j.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            Integer value = queue.poll();
+                            System.out.println(value);
+                            if (value == null) {
+                                break;
+                            }
+
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
